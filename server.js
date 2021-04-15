43e1cb15506e671fbe6f85073dcaac77452d893d
mdeepak98 <67814733+mdeepak98@users.noreply.github.com>
2021-04-16 01:23:16 +0530

    Enabling playground in production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolver.js";
 const PORT = process.env.PORT || 5000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers,playground: true });
 server.listen(PORT).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
