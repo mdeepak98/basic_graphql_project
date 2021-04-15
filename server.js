951b1e248c80a80e95cc3a078b26841f75c06164
mdeepak98 <67814733+mdeepak98@users.noreply.github.com>
2021-04-16 01:10:50 +0530

    changing server running port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolver.js";
-
+const PORT = 5000;
 
 const server = new ApolloServer({ typeDefs, resolvers });
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
