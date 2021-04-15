f73bb674f6a25f78f2784384784cda42bb3b5d37
mdeepak98 <67814733+mdeepak98@users.noreply.github.com>
2021-04-16 01:15:04 +0530

    updating to use port from process env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolver.js";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen(PORT).then(({ url }) => {
